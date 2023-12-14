d27ed6cb50342b17905bac70df376c45adffeaea
Shamainb <shamainbhengesa@gmail.com>
2023-12-14 04:46:25 +0200

    task0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- script that lists all privileges of the MySQL users
+SELECT * FROM user WHERE User = 'user_0d_1' AND Host = 'localhost';
+SELECT * FROM user WHERE User = 'user_0d_2' AND Host = 'localhost';
