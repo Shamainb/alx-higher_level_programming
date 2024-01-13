9ca2930764cf29e780a578f024734b6126717b9b
Shamainb <shamainbhengesa@gmail.com>
2024-01-13 12:55:08 +0200

    task6

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- prints the full description of the table first_table in MySQL server.
-SHOW CREATE TABLE
-'first_table';
+SHOW CREATE TABLE first_table;
--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,2 @@
 -- lists all rows of the table first_table in MySQL server.
-SELECT * FROM
-'first_table';
+SELECT * FROM first_table;
